b30eab2b909e1df9bcea2a2198b61b906facebca
Usman Irale <superirale@gmail.com>
2017-01-06 01:01:13 +0100

    capistrano config for deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,27 +1,27 @@
 # config valid only for current version of Capistrano
 lock '3.6.1'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'panel'
+set :repo_url, 'https://github.com/superirale/panel.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-
+set :branch, "master"
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
+set :deploy_to, '/var/www/html/panel'
 
 # Default value for :scm is :git
-# set :scm, :git
+set :scm, :git
 
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
+# Default value for :format is :pretty
+set :format, :pretty
 
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: 'log/capistrano.log', color: :auto, truncate: :auto
+# Default value for :log_level is :debug
+# set :log_level, :debug
 
 # Default value for :pty is false
-# set :pty, true
+set :pty, true
+set :use_sudo, false
 
 # Default value for :linked_files is []
 # append :linked_files, 'config/database.yml', 'config/secrets.yml'
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -59,3 +59,35 @@
 #     auth_methods: %w(publickey password)
 #     # password: 'please use keys'
 #   }
+role :app, %w{deployer@185.121.170.150}
+server '185.121.170.150', user: 'deployer', roles: %w{app}
+
+set :ssh_options, {
+    forward_agent: false,
+    auth_methods: %w(password),
+    password: 'omokhudu',
+    user: 'deployer',
+}
+
+
+namespace :deploy do
+    desc "Build"
+    after :updated, :build do
+        on roles(:app) do
+            within release_path  do
+            	execute "cp #{release_path}/.env.demo #{release_path}/.env"
+                execute :composer, "install --no-dev" # install dependencies
+                execute :composer, "dump-autoload -o" # reload autoload file dependencies
+                execute "php #{release_path}/artisan key:generate"
+                execute "chmod -R 777 #{release_path}/storage"
+                execute "chmod -R 777 #{release_path}/bootstrap/cache"
+                execute "chmod -R 777 #{release_path}/vendor"
+                execute "chmod -R 777 #{release_path}/public"
+                execute "php #{release_path}/artisan migrate"
+                # execute "php #{release_path}/artisan cache:clear"
+            end
+        end
+    end
+end
+
+
